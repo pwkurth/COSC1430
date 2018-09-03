e3f2874bd4bdc55953c9b1538545db800e2fbe70
pwkurth <38272066+pwkurth@users.noreply.github.com>
2018-09-03 02:22:51 -0500

    Update 9/3/2018

--- COSCHomework/main.cpp
+++ COSCHomework/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream> 
 #include <stdlib.h>
 
+
 using namespace std;
 
 
@@ -11,16 +12,29 @@ int main()
 	const int size = 10;
 	double classA[size];
 	double classB[size];
+	double sumA = 0;
+	double sumB = 0;
+	double avgA = 0;
+	double avgB = 0;
 	int seed_value;
 	
 	cin >> seed_value;
 	srand(seed_value); 
 		for (int i = 0; i < size; i++) {
-			classA[i] = rand() %100;
-			srand();
+			classA[i] = rand() %101;
+			classB[i] = rand() %101;
+
 			cout << classA[i] << endl;
-	}
-	
+			cout << classB[i] << endl;
+			sumA += size;
+	}		
+			for(int i=0; i<size; i++){
+				sumA += classA[i];
+				avgA += 1;
+				avgA = sumA / size;
+			}
+		cout << avgA << endl;
+
 	system("PAUSE");
 
 	return 0;
